42367d305afc5f736026ebdd09ad46e50df35fdb
Micah <mrmwgames@gmail.com>
2025-11-17 19:56:50 +1100

    Update database path to sqlite:///test.db

--- scripts/config.py
+++ scripts/config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/test.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REQUIRE_JOIN_CODE = os.environ.get('REQUIRE_JOIN_CODE', 'False').lower() == 'true'
     JOIN_CODE = os.environ.get('JOIN_CODE') if REQUIRE_JOIN_CODE else None
