ffb5f8e730d7db1e75108545ae0d18091814cdb1
Micah <mrmwgames@gmail.com>
2025-11-25 10:56:54 +1100

    feat: Add sorting and filtering to all tables
    This commit integrates the DataTables.js library to provide sorting, filtering, and pagination for all tables in the application, including the admin panel and the scoreboard.
    
    This addresses the user's request to add sorting and filtering to all tables.

---
+++ static/js/tables.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#users-table').DataTable();
+    $('#challenges-table').DataTable();
+    $('#categories-table').DataTable();
+    $('#award-categories-table').DataTable();
+    $('#submissions-table').DataTable();
+    $('#playerRankings').DataTable();
+});
